04dac8de70b3a98de24fd980597a54d14cef5366
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2023-07-21 17:15:37 +0900

    7일차 개발 11 / 22

--- Assets/Undead Survivor/Code/Bullet.cs
+++ Assets/Undead Survivor/Code/Bullet.cs
@@ -7,9 +7,39 @@ public class Bullet : MonoBehaviour
     public float damage;
     public int per;
 
-    public void Init(float damage, int per)
+    Rigidbody2D rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();    
+    }
+
+    public void Init(float damage, int per, Vector3 dir)
     {
         this.damage = damage;
         this.per = per;
+
+        if(per > -1)
+        {
+            rigid.velocity = dir * 15f;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // EnemyTag 만 다룬다
+        if (!collision.CompareTag("Enemy") || per == -1)
+        {
+            return;
+        }
+
+        per--;
+
+        if(per == -1)
+        {
+            // 물리 이므로 Vecotr2, 관통
+            rigid.velocity = Vector2.zero;
+            gameObject.SetActive(false);
+        }
     }
 }
--- Assets/Undead Survivor/Code/Weapon.cs
+++ Assets/Undead Survivor/Code/Weapon.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,8 +9,15 @@ public class Weapon : MonoBehaviour
     public int prefabId;
     public float damage;
     public int count;
-    public int speed;
+    public float speed;
 
+    float timer;
+    Player player;
+
+    void Awake()
+    {
+      player = GetComponentInParent<Player>();  
+    }
 
     void Start()
     {
@@ -23,15 +31,23 @@ public class Weapon : MonoBehaviour
                 transform.Rotate(Vector3.back * speed * Time.deltaTime);
                 break;
             default:
+                timer += Time.deltaTime;
+
+                if(timer > speed)
+                {
+                    timer = 0f;
+                    Fire();
+                }
                 break;
         }
 
         // Test Code
         if (Input.GetButtonDown("Jump"))
         {
-            LevelUp(20, 5);
+            LevelUp(10, 1);
         }
     }
+
 
     public void LevelUp(float damage, int count)
     {
@@ -53,6 +69,7 @@ public class Weapon : MonoBehaviour
                 Batch();
                 break;
             default:
+                speed = 0.3f;
                 break;
         }
     }
@@ -82,7 +99,26 @@ public class Weapon : MonoBehaviour
             // Translate 함수로 자신의 위쪽으로 이동, 이동방향은 Space.World 기준
             bullet.Translate(bullet.up *  1.5f, Space.World);
 
-            bullet.GetComponent<Bullet>().Init(damage, -1); // -1 is Infinity Per.
+            bullet.GetComponent<Bullet>().Init(damage, -1, Vector3.zero); // -1 is Infinity Per.
         }
+    }
+
+    void Fire()
+    {
+        // 적이 있는지 없는지 확인
+        if (!player.scanner.nearestTarget)
+        {
+            return;
+        }
+
+        Vector3 targetPos = player.scanner.nearestTarget.position;
+        Vector3 dir = targetPos - transform.position;
+        dir = dir.normalized;
+
+        Transform bullet = GameManager.instance.pool.Get(prefabId).transform;
+        bullet.position = transform.position;
+        // 지정된 축을 중심으로 목표를 향해 회전하는 함수
+        bullet.rotation = Quaternion.FromToRotation(Vector3.up, dir);
+        bullet.GetComponent<Bullet>().Init(damage, count, dir); 
     }
 }
